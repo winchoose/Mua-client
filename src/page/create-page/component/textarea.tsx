1acf89f56b74ff34e478fa035899f3d4b134b5cc
winchoose <tmdxor741@naver.com>
2026-01-21 11:29:29 +0900

    feat: textarea 컴포넌트 구현

---
+++ src/page/create-page/component/textarea.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@shared/utils/cn';
+import { cva } from 'class-variance-authority';
+import type { ComponentProps } from 'react';
+
+type TextareaProps = ComponentProps<'textarea'>;
+
+const textVariants = cva(`
+    w-[32.7rem]
+    h-[20rem]
+    text-body1
+    resize-none
+    border
+    border-gray-200
+    rounded-[16px]
+    transition-colors
+    outline-none
+    p-[1.6rem]
+    focus:border-primary
+    focus:text-gray-950
+    [&:not(:focus):not(:placeholder-shown)]:border-gray-950
+    [&:not(:focus):not(:placeholder-shown)]:text-gray-950
+    placeholder:text-gray-300
+`);
+
+export function Textarea({ className, ...props }: TextareaProps) {
+  return <textarea className={cn(textVariants(), className)} {...props} />;
+}
