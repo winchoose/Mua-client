4e2470a2141e97b2c77f2650dfeb9f8f2a97ee25
winchoose <tmdxor741@naver.com>
2026-02-11 10:21:49 +0900

    fix: 쓰지않는 코드 삭제

--- src/page/post-detail-page.tsx
+++ src/page/post-detail-page.tsx
@@ -1,5 +1,5 @@
 import { TopNavigation } from '@shared/ui/topNavigation';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import ArrowLeftIcon from '@shared/assets/icon/arrow-left.svg?react';
 import UploadIcon from '@shared/assets/icon/upload.svg?react';
 import { Carousel } from '@widgets/postDetail/carousel/carousel';
